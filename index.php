b66652050eafca4175dff9af598fb325c61786f4
Beizeng <50338897+BeizengVue92@users.noreply.github.com>
2023-04-29 12:59:36 -0500

    Making index.html into index.php

--- index.php
+++ index.php
@@ -1,3 +1,113 @@
-<?php
-echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Beizeng Vue's Portfolio</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Top Navigation Menu -->
+  <div class="topnav">
+    <a href="#home" class="active">
+      <h2>Beizeng Vue's Portfolio</h2>
+    </a>
+    <!-- Navigation links (hidden by default) -->
+    <div id="myLinks">
+      <a href="index.html">Home</a>
+      <a href="projects.html">Projects</a>
+      <a href="contact.html">Contact</a>
+    </div>
+    <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col">
+
+        <div class="imageContainer">
+          <img src="images/profilepicture.jpg" alt="Profile Picture" class="profilePicture">
+        </div>
+
+      </div>
+
+      <div class="col">
+
+        <section class="summary">
+          <h5>
+            Hello, my name is Beizeng Vue and I am a Software Developer and a current student at Chippewa Valley
+            Technical College
+            going for their Associate of Applied Science in Information Technology. As a beginner software
+            developer,
+            my
+            portfolio website
+            showcases my skills, experience, and projects to potential employers and clients. I have a strong
+            foundation
+            in programming
+            concepts and languages, including PHP, JavaScript, ASP.Net, and Java. I have built several practical
+            projects
+            to gain hands-on
+            experience and apply what I've learned. In addition to my technical skills, I am a quick learner, team
+            player, and excellent
+            communicator. I am eager to collaborate with others on projects and contribute to the development
+            community.
+            Feel free to contact
+            me with any questions or opportunities.
+          </h5>
+        </section>
+
+      </div>
+
+    </div>
+
+    <section class="skills">
+
+      <h2>Skills</h2>
+
+      <div class="row">
+
+        <div class="col">
+
+          <img class="logo" src="images/htmlLogo.png" alt="HTML5 Logo">
+          <img class="logo" src="images/cssLogo.png" alt="CSS Logo">
+          <img class="logo" src="images/jsLogo.png" alt="JavaScript Logo">
+          <img class="logo" src="images/bootstrapLogo.png" alt="BootStrap 4 Logo">
+          <img class="logo" src="images/javaLogo.png" alt="Java Logo">
+          <img class="logo" src="images/phpLogo.png" alt="PHP Logo">
+          <img class="logo" src="images/csharpLogo.png" alt="C Sharp Logo">
+
+        </div>
+
+      </div>
+
+    </section>
+
+  </div>
+
+  </div>
+
+
+  <footer>
+    <p>Copyright &copy; 2023 Beizeng Vue</p>
+  </footer>
+
+  <script src="js/nav.js"></script>
+  <script src="js/jquery.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
